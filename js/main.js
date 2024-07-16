7bdc4e097b757b3873f1caa39471901c3611f457
ViraMesha <wira.mescha@gmail.com>
2024-07-16 21:45:58 +0300

    feat: Add reviews section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 22,
+  navigation: {
+    nextEl: ".reviews__arrow-next",
+    prevEl: ".reviews__arrow-prev",
+  },
+  loop: true,
+});
