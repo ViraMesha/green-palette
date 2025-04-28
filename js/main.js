988d773d42a1ebb93a0fb27ca4afa75fe9666994
ViraMesha <wira.mescha@gmail.com>
2025-04-28 13:01:20 +0300

    feat: Write js for accordion

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ const menuList = document.querySelector(".menu__list");
 menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("menu--open");
 });
+
+const accordionTitle = document.querySelectorAll(".accordion__title");
+accordionTitle.forEach((item) => {
+  item.addEventListener("click", () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains("accordion__item--active")) {
+      parent.classList.remove("accordion__item--active");
+    } else {
+      accordionTitle.forEach((item) =>
+        item.parentNode.classList.remove("accordion__item--active")
+      );
+
+      parent.classList.add("accordion__item--active");
+    }
+  });
+});
